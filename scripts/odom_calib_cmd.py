2e1636759f0177ded09078c9512dfb5148b99fd5
DomBaril <dominic.baril@usherbrooke.ca>
2020-10-19 10:44:28 -0400

    corrected copy mistake in script

--- scripts/odom_calib_cmd.py
+++ scripts/odom_calib_cmd.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-import rospy
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Joy
-
-import numpy as np
-
-dead_man = 0
-dead_man_index = 0
-
-def callback(data):
-    global dead#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
