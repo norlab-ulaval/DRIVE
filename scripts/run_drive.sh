6fa12bee6e20dec6992f0bb82373c2a6d8a1d66f
DomBaril <dominic.baril@usherbrooke.ca>
2023-09-29 13:45:03 -0400

    progress on script automating gathering and training

--- scripts/run_drive.sh
+++ scripts/run_drive.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-(ros2 launch drive drive.launch.xml)
-pid=$!
-printf $pid
+echo -n "Enter experiment name: "
+read experiment_name
+
+ros2 launch drive drive.launch.xml &
+sleep 2
+read -p "When dataset gathering is done, press Enter to save"
+
+export_path="${PWD}/../${experiment_name}/"
+
+if [ ! -d $export_path ]; then
+  mkdir -p $export_path;
+fi
+
+srv_string="export_path: data: '${export_path}raw_dataframe.pkl'"
+echo -n "${srv_string}"
+
+ros2 service call /logger_node/export_data norlab_controllers_msgs/srv/ExportData "${srv_string}"
+
+sleep 5
+
+killall drive_node
+killall logger_node
