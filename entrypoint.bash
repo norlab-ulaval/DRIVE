a8b7485780f13ed25463ac166050daeca4f37b0e
William Fecteau <william-fecteau@hotmail.com>
2025-07-09 12:02:04 -0400

    Added a prod build stage to run on robot

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+source /opt/ros/$ROS_DISTRO/setup.bash
+source /home/ws/install/setup.bash
+
 screen -wipe > /dev/null 2>&1
 killall screen > /dev/null 2>&1
 
 screen -S foxglove -dm bash -c 'ros2 launch foxglove_bridge foxglove_bridge_launch.xml'
-screen -S drive -dm bash -c 'ros2 launch drive_ros sim_demo.launch.py'
+
+echo "ROS screens launched, container is ready"
+while true; do sleep 1; done
