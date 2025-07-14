f41e5b5f216540979bf7a01c40b7dfee56dadec2
William Fectau <william-fecteau@hotmail.com>
2025-07-14 13:29:16 -0400

    Fix permission when drive_datasets folder is freshly created by root

--- entrypoint.bash
+++ entrypoint.bash
@@ -6,6 +6,10 @@ source /home/ws/install/setup.bash
 screen -wipe > /dev/null 2>&1
 killall screen > /dev/null 2>&1
 
+if [ -d /home/ws/drive_datasets ]; then
+    sudo chown -R $(id -u):$(id -g) /home/ws/drive_datasets
+fi
+
 screen -S foxglove -dm bash -c 'ros2 launch foxglove_bridge foxglove_bridge_launch.xml'
 
 echo "ROS screens launched, container is ready"
--- ros/drive_ros/node_utils.py
+++ ros/drive_ros/node_utils.py
@@ -7,7 +7,7 @@ def declare_parameter_from_dataclass(node: Node, dataclass_inst):
         default_value = dataclass_inst.__getattribute__(field_name)
 
         node.declare_parameter(field_name, default_value)
-    
+
     # Make sure we update the values if the default are overwritten with a config/launch file
     update_parameter_from_dataclass(node, dataclass_inst)
 
