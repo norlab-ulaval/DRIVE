2cc4e4cc45ef95b61316e8cc1002f93cb63aa47c
William Fectau <william-fecteau@hotmail.com>
2025-07-30 18:13:55 +0000

    Added lazygit in container

--- .devcontainer/on_create.bash
+++ .devcontainer/on_create.bash
@@ -5,3 +5,11 @@ pip install -e .
 sudo apt-get update -y
 sudo rosdep update
 sudo rosdep install --from-paths ros --ignore-src -y
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit*
+
