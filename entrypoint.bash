7b5f872fc4e161bad718eaf4873a1d418815aa01
William Fectau <william-fecteau@hotmail.com>
2025-08-04 13:46:16 -0400

    Added automatic drive launch on entrypoint

--- entrypoint.bash
+++ entrypoint.bash
@@ -6,11 +6,17 @@ source /home/ws/install/setup.bash
 screen -wipe > /dev/null 2>&1
 killall screen > /dev/null 2>&1
 
+if [ -z "$ROBOT_NAME" ]; then
+  echo "Error: ROBOT_NAME is not set. Please set it in your environment or .env file."
+  exit 1
+fi
+
 if [ -d /home/ws/drive_datasets ]; then
     sudo chown -R $(id -u):$(id -g) /home/ws/drive_datasets
 fi
 
-screen -S foxglove -dm bash -c 'ros2 launch foxglove_bridge foxglove_bridge_launch.xml'
+# Uncomment if foxglove is not launched on the host robot
+# screen -S foxglove -dm bash -c 'ros2 launch foxglove_bridge foxglove_bridge_launch.xml'
 
-echo "ROS screens launched, container is ready"
+ros2 launch drive_ros $ROBOT_NAME.launch.py
 while true; do sleep 1; done
