04f08948e9cb4131edbec8271b352f85209dd9df
William Fectau <william-fecteau@hotmail.com>
2025-10-09 20:27:57 +0000

    Fixed some bug in the prediction and export scripts

--- scripts/motion_models/model.py
+++ scripts/motion_models/model.py
@@ -4,8 +4,11 @@ from typing import Callable
 from matplotlib import pyplot as plt
 import numpy as np
 
-StateDiffFunction = Callable[[np.ndarray, np.ndarray], np.ndarray] # f(x, u)
-IntegratorFunction = Callable[[StateDiffFunction, np.ndarray, np.ndarray, float], np.ndarray] # x_next = f(dstate, x, u, dt)
+StateDiffFunction = Callable[[np.ndarray, np.ndarray], np.ndarray]  # f(x, u)
+IntegratorFunction = Callable[
+    [StateDiffFunction, np.ndarray, np.ndarray, float], np.ndarray
+]  # x_next = f(dstate, x, u, dt)
+
 
 class MotionModel(ABC):
     @abstractmethod
@@ -15,18 +18,20 @@ class MotionModel(ABC):
 
 def rk4(f: StateDiffFunction, x: np.ndarray, u: np.ndarray, dt: float) -> np.ndarray:
     k1 = f(x, u)
-    k2 = f(x + 0.5*dt*k1, u)
-    k3 = f(x + 0.5*dt*k2, u)
-    k4 = f(x + dt*k3, u)
-    return x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
+    k2 = f(x + 0.5 * dt * k1, u)
+    k3 = f(x + 0.5 * dt * k2, u)
+    k4 = f(x + dt * k3, u)
+    return x + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
 
 def euler(f: StateDiffFunction, x: np.ndarray, u: np.ndarray, dt: float) -> np.ndarray:
     return x + dt * f(x, u)
 
+
 class IdealDiffDrive(MotionModel):
     """
     Ideal differential drive model.
-    
+
     State
     ----------
     [x, y, theta] (m, m, rad)
@@ -36,6 +41,7 @@ class IdealDiffDrive(MotionModel):
     -----------
     [left_wheel_angular_speed, right_wheel_angular_speed] (rad/s, rad/s).
     """
+
     def __init__(self, wheelbase, wheel_radius):
         """
         wheelbase: (m)
@@ -51,8 +57,7 @@ class IdealDiffDrive(MotionModel):
             ]
         )
         self.inv_J = np.linalg.inv(self.J)
-    
-    
+
     def dstate(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
         """
         Compute the time derivative of the system state.
@@ -74,14 +79,12 @@ class IdealDiffDrive(MotionModel):
         v, omega = (self.J @ u).flatten()
         theta = x[2]
 
-        return np.array([
-            v * np.cos(theta),
-            v * np.sin(theta),
-            omega
-        ])
+        return np.array([v * np.cos(theta), v * np.sin(theta), omega])
 
 
-def predict(model: MotionModel, x_0: np.ndarray, u_arr: np.ndarray, dt: float, integrator: IntegratorFunction) -> np.ndarray:
+def predict(
+    model: MotionModel, x_0: np.ndarray, u_arr: np.ndarray, dt: float, integrator: IntegratorFunction
+) -> np.ndarray:
     """
     Propagate the system state over time given control inputs.
 
@@ -103,7 +106,7 @@ def predict(model: MotionModel, x_0: np.ndarray, u_arr: np.ndarray, dt: float, i
     """
 
     N = u_arr.shape[1]
-    x_arr = np.zeros((3, N+1))
+    x_arr = np.zeros((3, N + 1))
     x_arr[:, 0] = x_0[:, 0]
 
     for i in range(N):
@@ -112,22 +115,23 @@ def predict(model: MotionModel, x_0: np.ndarray, u_arr: np.ndarray, dt: float, i
 
         x_next = integrator(model.dstate, x, u, dt)
         x_arr[:, i + 1] = x_next
-        
+
     return x_arr
+
 
 if __name__ == "__main__":
     model = IdealDiffDrive(wheelbase=1.5, wheel_radius=1.0)
 
-    x0 = np.zeros((3,1))
+    x0 = np.zeros((3, 1))
     dt = 0.1
     N = int(6.0 / dt)
     u = np.array([1.0, 2.0])
     u_arr = np.column_stack([u for _ in range(N)])
-    
+
     x_arr = predict(model, x0, u_arr, dt, rk4)
     x_euler_arr = predict(model, x0, u_arr, dt, euler)
 
     plt.scatter(x_arr[0, :], x_arr[1, :])
-    plt.scatter(x_euler_arr[0, :], x_euler_arr[1, :], color='red')
+    plt.scatter(x_euler_arr[0, :], x_euler_arr[1, :], color="red")
     plt.axis("equal")
     plt.show()
--- scripts/prediction.py
+++ scripts/prediction.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from motion_models.model import IdealDiffDrive, rk4, predict
 
+
 def get_data_between_timestamps(
     dataframe: pd.DataFrame, timestamp_field_name: str, start_timestamp: float, end_timestamp: float
 ):
@@ -14,8 +15,14 @@ def get_data_between_timestamps(
     return dataframe.loc[mask].reset_index(drop=True)
 
 
-data_dir = Path("../drive_datasets/old_drive/warthog/wheels/grass/warthog_wheels_grass_2024_9_20_9h9s5/model_training_datasets/new_drive_format")
-# data_dir = Path("../drive_datasets/warthog/micro_drive_grass")
+# data_dir = Path(
+#     "../drive_datasets/old_drive/warthog/wheels/sand/warthog_wheels_sand_2024_9_25_14h51s17/model_training_datasets/new_drive_format"
+# )
+# data_dir = Path(
+#     "../drive_datasets/old_drive/warthog/wheels/grass/warthog_wheels_grass_2024_9_20_9h27s52/model_training_datasets/new_drive_format"
+# )
+data_dir = Path("../drive_datasets/warthog/micro_drive_grass")
+# data_dir = Path("../drive_datasets/2025-10-09_16-05-04")
 
 positions = pd.read_csv(data_dir / "positions.csv")
 steps = pd.read_csv(data_dir / "steps.csv")
@@ -23,31 +30,38 @@ steps = pd.read_csv(data_dir / "steps.csv")
 completed_steps = steps[steps["completion_status"] == "completed"]
 
 for i, step in completed_steps.iterrows():
-    start_timestamp = step["start_timestamp"]
+    start_timestamp = step["end_timestamp"] - 2 * 1e9
     end_timestamp = step["end_timestamp"]
 
     positions_icp = get_data_between_timestamps(positions, "timestamp", start_timestamp, end_timestamp)
 
-    x_0 = positions_icp.iloc[0][["x", "y", "yaw"]].to_numpy().reshape((3,1))
+    x_0 = positions_icp.iloc[0][["x", "y", "yaw"]].to_numpy().reshape((3, 1))
 
     v = step["commanded_linear_velocity"]
     omega = step["commanded_angular_velocity"]
-    u = np.array([v, omega])
+    body_u = np.array([v, omega])
+    model = IdealDiffDrive(wheelbase=1.08, wheel_radius=0.3)
+
+    if abs(v) > 3 or abs(omega) > 3:
+        print(f"Skipping step {i} with v={v:.2f} m/s, omega={omega:.2f} rad/s")
+        continue
+
+    u = (model.inv_J @ body_u.T).flatten()
 
     dt = 0.1
-    N = int(6.0 / dt)
+
+    time = (end_timestamp - start_timestamp) * 1e-9
+    N = int(time / dt)
 
     u_arr = np.column_stack([u for _ in range(N)])
 
-    model = IdealDiffDrive(wheelbase=1.08, wheel_radius=0.3)
     x_arr = predict(model, x_0, u_arr, dt, rk4)
 
     plt.scatter(positions_icp["x"], positions_icp["y"], label=f"step {i}")
-    plt.scatter(x_arr[0, :], x_arr[1, :], label=f"model {i}", color='red')
-    plt.scatter(x_0[0], x_0[1], color='green', label="start", s=100)
+    plt.scatter(x_arr[0, :], x_arr[1, :], label=f"IDD", color="red")
+    plt.scatter(x_0[0], x_0[1], color="green", label="start", s=100)
+    plt.title(f"Step {i} - v={v:.2f} m/s, omega={omega:.2f} rad/s")
     plt.legend()
     plt.axis("equal")
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
+    plt.grid()
     plt.show()
-    
